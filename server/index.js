b49fcf2d4137622b18d26b75b6f4fc1c170f0c81
Billy <billy.m.nightingale@gmail.com>
2022-07-20 16:04:13 +0200

    set-up and connected to the DB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 require("dotenv").config();
+const connection = require("../db-config.js");
 
 const port = process.env.PORT ?? 5000;
+
+connection.connect((err) => {
+    if (err) {
+        console.error('error connecting' + err.stack);
+    } else {
+        console.log('connected to the database with threadId: ' + connection.threadId);
+    }
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
