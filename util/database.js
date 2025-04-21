0e9981e1cdec3468a5ed7fc9cf6840175d73f99f
Billy <billy.m.nightingale@gmail.com>
2025-04-21 22:46:38 +0200

    updated error handling for db connection

--- util/database.js
+++ util/database.js
@@ -1,12 +1,23 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
+console.log("Starting server...");
+
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQLDATABASE,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err.stack);
+    process.exit(1);
+  } else {
+    console.log("Database connected!");
+  }
 });
 
 module.exports = pool;
