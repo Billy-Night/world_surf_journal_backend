972d4296480553a85d71068468b2e1f9d6e311ad
Billy <billy.m.nightingale@gmail.com>
2025-04-22 22:37:53 +0200

    update

--- app.js
+++ app.js
@@ -8,18 +8,11 @@ const tripsRoutes = require("./routes/trips.js");
 
 const corsOptions = {
   origin: process.env.ORIGIN_RAIL,
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
-
-// app.use((req, res, next) => {
-//   if (req.method === "OPTIONS") {
-//     res.sendStatus(200);
-//   } else {
-//     next();
-//   }
-// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
